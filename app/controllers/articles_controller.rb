14578574fc75d221a0068030c8285e312f29ac93
Jorge Carmine <george_gamez@hotmail.com>
2022-11-20 20:04:04 -0600

    Index articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
   # before_action :find_article, only: [:show, :edit, :update, :destroy]
-  before_action :find_article, except: [:new, :create]
+  before_action :find_article, except: [:index, :new, :create]
 
   def index
     @articles = Article.all
